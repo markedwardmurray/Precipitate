396be1e1cc84b32cb620cdf767c9f74f35a83280
markedwardmurray <markedwardmurray@gmail.com>
2015-11-25 18:24:05 -0500

    add APIKey and API call

--- Precipitate/ViewController.swift
+++ Precipitate/ViewController.swift
@@ -7,12 +7,23 @@
 //
 
 import UIKit
+import ForecastIO
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let forecastClient = APIClient(apiKey: marksAPIKey)
+        
+        forecastClient.getForecast(latitude: 40.772768, longitude: -73.915739) { (currentForecast, error) -> Void in
+            if error != nil {
+                print(error)
+            } else {
+                print(currentForecast)
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +31,7 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    
 }
 
