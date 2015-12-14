1c6ab2f16cd15853171134560bf0ee6ea3312398
markedwardmurray <markedwardmurray@gmail.com>
2015-12-14 15:09:11 -0500

    add SummaryVC to storyboard layout

---
+++ Precipitate/SummaryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SummaryViewController.swift
+//  Precipitate
+//
+//  Created by Mark Murray on 12/14/15.
+//  Copyright © 2015 markedwardmurray. All rights reserved.
+//
+
+import UIKit
+
+class SummaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
