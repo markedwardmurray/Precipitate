952353377188d2968aa971b39b72bb63c8f4e6b1
markedwardmurray <markedwardmurray@gmail.com>
2015-12-15 01:55:27 -0500

    reload InitialVC when app returns to active

--- Precipitate/AppDelegate.swift
+++ Precipitate/AppDelegate.swift
@@ -36,6 +36,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        
+        if let initialVC = self.window?.rootViewController as? InitialViewController {
+            initialVC.viewDidAppear(true)
+        }
     }
 
     func applicationWillTerminate(application: UIApplication) {
