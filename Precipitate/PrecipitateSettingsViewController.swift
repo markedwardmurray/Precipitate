6012d6363ba0348a59c749064632cbcec243a896
markedwardmurray <markedwardmurray@gmail.com>
2016-03-05 00:50:58 -0500

    [lang] alphabetize language options in settings menu

--- Precipitate/PrecipitateSettingsViewController.swift
+++ Precipitate/PrecipitateSettingsViewController.swift
@@ -29,31 +29,36 @@ class PrecipitateSettingsViewController: SwiftySettingsViewController {
         /* Language option objects */
         let ar010 = ForecastLanguage(option: ForecastLanguageOption.Arabic)
         let bs020 = ForecastLanguage(option: ForecastLanguageOption.Bosnian)
+        let zh260 = ForecastLanguage(option: ForecastLanguageOption.Chinese)
+        let zhtw270 = ForecastLanguage(option: ForecastLanguageOption.ChineseTW)
+        let kw130 = ForecastLanguage(option: ForecastLanguageOption.Cornish)
+        
+        let hr090 = ForecastLanguage(option: ForecastLanguageOption.Croatian)
         let cs030 = ForecastLanguage(option: ForecastLanguageOption.Czech)
+        let nl150 = ForecastLanguage(option: ForecastLanguageOption.Dutch)
+        let en060 = ForecastLanguage(option: ForecastLanguageOption.English)
+        let fr080 = ForecastLanguage(option: ForecastLanguageOption.French)
+        
         let de040 = ForecastLanguage(option: ForecastLanguageOption.German)
         let el050 = ForecastLanguage(option: ForecastLanguageOption.Greek)
-        let en060 = ForecastLanguage(option: ForecastLanguageOption.English)
-        let es070 = ForecastLanguage(option: ForecastLanguageOption.Spanish)
-        let fr080 = ForecastLanguage(option: ForecastLanguageOption.French)
-        let hr090 = ForecastLanguage(option: ForecastLanguageOption.Croatian)
         let hu100 = ForecastLanguage(option: ForecastLanguageOption.Hungarian)
         let it110 = ForecastLanguage(option: ForecastLanguageOption.Italian)
         let is120 = ForecastLanguage(option: ForecastLanguageOption.Icelandic)
-        let kw130 = ForecastLanguage(option: ForecastLanguageOption.Cornish)
+        
+        let pig250 = ForecastLanguage(option: ForecastLanguageOption.PigLatin)
         let nb140 = ForecastLanguage(option: ForecastLanguageOption.Norwegian)
-        let nl150 = ForecastLanguage(option: ForecastLanguageOption.Dutch)
         let pl160 = ForecastLanguage(option: ForecastLanguageOption.Polish)
         let pt170 = ForecastLanguage(option: ForecastLanguageOption.Portuguese)
         let ru180 = ForecastLanguage(option: ForecastLanguageOption.Russian)
-        let sk190 = ForecastLanguage(option: ForecastLanguageOption.Slovak)
+        
         let sr200 = ForecastLanguage(option: ForecastLanguageOption.Serbian)
+        let sk190 = ForecastLanguage(option: ForecastLanguageOption.Slovak)
+        let es070 = ForecastLanguage(option: ForecastLanguageOption.Spanish)
         let sv210 = ForecastLanguage(option: ForecastLanguageOption.Swedish)
         let tet220 = ForecastLanguage(option: ForecastLanguageOption.Tetum)
+        
         let tr230 = ForecastLanguage(option: ForecastLanguageOption.Turkish)
         let uk240 = ForecastLanguage(option: ForecastLanguageOption.Ukrainian)
-        let pig250 = ForecastLanguage(option: ForecastLanguageOption.PigLatin)
-        let zh260 = ForecastLanguage(option: ForecastLanguageOption.Chinese)
-        let zhtw270 = ForecastLanguage(option: ForecastLanguageOption.ChineseTW)
         
         /* Top Down settings */
         settings = SwiftySettings(storage: storage, title: "Settings") {
@@ -68,31 +73,36 @@ class PrecipitateSettingsViewController: SwiftySettingsViewController {
                     OptionsButton(key: "lang", title: "Language") {
                     [   Option(title: ar010.settingsDescription, optionId: ar010.option.rawValue),
                         Option(title: bs020.settingsDescription, optionId: bs020.option.rawValue),
+                        Option(title: zh260.settingsDescription, optionId: zh260.option.rawValue),
+                        Option(title: zhtw270.settingsDescription, optionId: zhtw270.option.rawValue),
+                        Option(title: kw130.settingsDescription, optionId: kw130.option.rawValue),
+                        
+                        Option(title: hr090.settingsDescription, optionId: hr090.option.rawValue),
                         Option(title: cs030.settingsDescription, optionId: cs030.option.rawValue),
+                        Option(title: nl150.settingsDescription, optionId: nl150.option.rawValue),
+                        Option(title: en060.settingsDescription, optionId: en060.option.rawValue),
+                        Option(title: fr080.settingsDescription, optionId: fr080.option.rawValue),
+                        
                         Option(title: de040.settingsDescription, optionId: de040.option.rawValue),
                         Option(title: el050.settingsDescription, optionId: el050.option.rawValue),
-                        Option(title: en060.settingsDescription, optionId: en060.option.rawValue),
-                        Option(title: es070.settingsDescription, optionId: es070.option.rawValue),
-                        Option(title: fr080.settingsDescription, optionId: fr080.option.rawValue),
-                        Option(title: hr090.settingsDescription, optionId: hr090.option.rawValue),
                         Option(title: hu100.settingsDescription, optionId: hu100.option.rawValue),
                         Option(title: it110.settingsDescription, optionId: it110.option.rawValue),
                         Option(title: is120.settingsDescription, optionId: is120.option.rawValue),
-                        Option(title: kw130.settingsDescription, optionId: kw130.option.rawValue),
+                        
+                        Option(title: pig250.settingsDescription, optionId: pig250.option.rawValue),
                         Option(title: nb140.settingsDescription, optionId: nb140.option.rawValue),
-                        Option(title: nl150.settingsDescription, optionId: nl150.option.rawValue),
                         Option(title: pl160.settingsDescription, optionId: pl160.option.rawValue),
                         Option(title: pt170.settingsDescription, optionId: pt170.option.rawValue),
                         Option(title: ru180.settingsDescription, optionId: ru180.option.rawValue),
-                        Option(title: sk190.settingsDescription, optionId: sk190.option.rawValue),
+                        
                         Option(title: sr200.settingsDescription, optionId: sr200.option.rawValue),
+                        Option(title: sk190.settingsDescription, optionId: sk190.option.rawValue),
+                        Option(title: es070.settingsDescription, optionId: es070.option.rawValue),
                         Option(title: sv210.settingsDescription, optionId: sv210.option.rawValue),
                         Option(title: tet220.settingsDescription, optionId: tet220.option.rawValue),
+                        
                         Option(title: tr230.settingsDescription, optionId: tr230.option.rawValue),
-                        Option(title: uk240.settingsDescription, optionId: uk240.option.rawValue),
-                        Option(title: pig250.settingsDescription, optionId: pig250.option.rawValue),
-                        Option(title: zh260.settingsDescription, optionId: zh260.option.rawValue),
-                        Option(title: zhtw270.settingsDescription, optionId: zhtw270.option.rawValue)
+                        Option(title: uk240.settingsDescription, optionId: uk240.option.rawValue)
                         ]
                     }
                 ]
