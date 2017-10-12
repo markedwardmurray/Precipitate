fb715b6532d68a204f182dee5acd3de3f145f08a
markedwardmurray <markedwardmurray@gmail.com>
2017-10-12 00:11:44 -0400

    pod init with Alamofire Anchorage DateToolsSwift

---
+++ Swift4/Precipitate/Pods/Target Support Files/Pods-Precipitate/Pods-Precipitate-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Precipitate : NSObject
+@end
+@implementation PodsDummy_Pods_Precipitate
+@end
---
+++ Swift4/Precipitate/Pods/Target Support Files/Pods-PrecipitateTests/Pods-PrecipitateTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PrecipitateTests : NSObject
+@end
+@implementation PodsDummy_Pods_PrecipitateTests
+@end
