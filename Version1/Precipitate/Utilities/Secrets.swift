dc5a0f7c5062194c2162d75ec96a8db58d3c98bf
markedwardmurray <markedwardmurray@gmail.com>
2022-06-25 15:36:04 -0400

    move everything old into Version1 dir

---
+++ Version1/Precipitate/Utilities/Secrets.swift
@@ -0,0 +1,12 @@
+//
+//  Secrets.swift
+//  Precipitate
+//
+//  Created by Mark Murray on 11/25/15.
+//  Copyright © 2015 markedwardmurray. All rights reserved.
+//
+
+import Foundation
+
+let marksAPIKey = "830e651695add40e74c4c5e8213c2892"
+
